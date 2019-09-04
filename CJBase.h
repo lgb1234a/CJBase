d7627d44829453a0af75c4e65ba055b254f167e3
chenyn <1048196441@qq.com>
2019-09-04 16:46:34 +0800

    zzlog

--- CJBase.h
+++ CJBase.h
@@ -6,3 +6,14 @@
 #import "NSString+Cajian.h"
 #import "UIViewController+HUD.h"
 #import "XTSafeCollection.h"
+
+
+#ifdef DEBUG
+
+#define ZZLog(...) NSLog(__VA_ARGS__)
+
+#else
+
+#define ZZLog(...)
+
+#endif
