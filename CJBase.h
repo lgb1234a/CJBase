fe10967cd97dc351c30683c277ef77e31ddf2c66
chenyn <1048196441@qq.com>
2019-09-26 16:14:59 +0800

    云信DEMO常用属性添加

--- CJBase.h
+++ CJBase.h
@@ -10,6 +10,42 @@
 #import "XTSafeCollection.h"
 
 
+static NSString *CJUpdateMessageNotification = @"CJUpdateMessageNotification";
+
+#define Notification_Font_Size   11   // 通知文字大小
+
+#define RGBACOLOR(r,g,b,a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)]
+
+/**
+ 主红色
+ */
+#define Main_redColor RGBACOLOR(216, 78, 67, 1)
+
+/**
+ 主背景色
+ */
+#define Main_BackgColor RGBACOLOR(236, 236, 236, 1)
+
+/**
+ 主字体偏黑
+ */
+#define Main_TextGrayColor RGBACOLOR(166, 166, 166, 1)
+
+/**
+ 主字体主黑
+ */
+#define Main_TextBlackColor RGBACOLOR(26, 26, 26, 1)
+
+/**
+ 导航背景色
+ */
+#define Main_NaviColor RGBACOLOR(236, 236, 236, 1)
+
+/**
+ 主线设置
+ */
+#define Main_lineColor RGBACOLOR(227, 226, 229, 1)
+
 #ifdef DEBUG
 
 #define ZZLog(...) NSLog(__VA_ARGS__)
@@ -19,3 +55,11 @@
 #define ZZLog(...)
 
 #endif
+
+#define CJ_SuppressPerformSelectorLeakWarning(Stuff) \
+do { \
+_Pragma("clang diagnostic push") \
+_Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
+Stuff; \
+_Pragma("clang diagnostic pop") \
+} while (0)
