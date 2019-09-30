707f2f3d45e3091a9aee94f9f1a59e7e747c46f8
chenyn <1048196441@qq.com>
2019-09-30 15:57:42 +0800

    自定义tf

---
+++ UIKit/CJTextField.h
@@ -0,0 +1,16 @@
+//
+//  CJTextField.h
+//  CJBase
+//
+//  Created by chenyn on 2019/9/30.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CJTextField : UITextField
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UIKit/CJTextField.m
@@ -0,0 +1,12 @@
+//
+//  CJTextField.m
+//  CJBase
+//
+//  Created by chenyn on 2019/9/30.
+//
+
+#import "CJTextField.h"
+
+@implementation CJTextField
+
+@end
